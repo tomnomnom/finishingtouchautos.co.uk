ac07b74b7a51ec41e059306c34dcd41661230254
Tom Hudson <mail@tomnomnom.com>
2015-12-12 10:12:44 -0500

    Initial

---
+++ public/index.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Finishing Touch Autos</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+        <meta name="theme-color" content="#0E35C1">
+
+        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Play:400,700"/>
+        <link rel="stylesheet" type="text/css" href="/css/main.css"/>
+    </head>
+
+    <body>
+        <header>
+            <a href="/">
+                <img src="/images/logo.png" alt="Finishing Touch Autos">
+            </a>
+        </header>
+
+        <nav>
+            <a href="/">Home</a>
+            <a href="/contact">Contact</a>
+            <a href="/gallery">Gallery</a>
+        </nav>
+        
+        <section class="intro">
+            <p>
+                We will do any modification for any vehicle, no matter how big or small.
+                We specialise in full re-paints aswell as mechanical repairs, services and routine
+                checks with all up-to-date technology for any make or type of vehicle.
+            </p>
+        </section>
+
+        <section class="cols">
+
+            <ul class="col">
+                <li>Custom Paintwork</li>
+                <li>Accident Repair</li>
+                <li>Custom Bodywork</li>
+                <li>Restoration</li>
+            </ul>
+            <ul class="col">
+                <li>ICE / Security</li>
+                <li>Servicing / Repairs</li>
+                <li>Diagnostics / Electrics</li>
+                <li>Kit Cars Built</li>
+            </ul>
+        </section>
+
+        <footer>
+            <p>&copy; Finishing Touch Autos 2015</p>
+        </footer>
+    </body>
+</html>
